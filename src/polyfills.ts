52043c76ab353502ca0e704a71217deaac4b9d96
mrcodewalker <mr.codewalker@gmail.com>
2024-08-11 23:33:05 +0700

    Update URL

---
+++ src/polyfills.ts
@@ -0,0 +1,26 @@
+/***************************************************************************************************
+ * BROWSER POLYFILLS
+ */
+
+/** IE11 requires the following for ngClass support on SVG elements */
+import 'classlist.js';  // Run `npm install classlist.js --save`
+
+/** IE10 and IE11 requires the following for ngIf/ngFor support on SVG elements */
+import 'svgxuse';  // Run `npm install svgxuse --save`
+
+/**
+ * Web Animations `@angular/platform-browser/animations`
+ * Required for: All Angular Material components
+ **/
+import 'web-animations-js';  // Run `npm install web-animations-js --save`
+
+/**
+ * Zone JS is required by Angular itself.
+ */
+import 'zone.js';  // Included with Angular CLI.
+
+/***************************************************************************************************
+ * APPLICATION IMPORTS
+ */
+
+/** Import other polyfills or app-specific code here */
