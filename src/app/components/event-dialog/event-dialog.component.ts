e1032a4a9431d63dd9a7d979b6042c9be66bb77c
mrcodewalker <mr.codewalker@gmail.com>
2024-08-19 22:16:56 +0700

    event Dialog

--- src/app/components/event-dialog/event-dialog.component.ts
+++ src/app/components/event-dialog/event-dialog.component.ts
@@ -7,10 +7,19 @@ import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
   styleUrls: ['./event-dialog.component.scss']
 })
 export class EventDialogComponent {
+  location: string = '';
+  teacher: string = '';
   constructor(
     public dialogRef: MatDialogRef<EventDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
-  ) {}
+  ) {
+      debugger;
+      this.location = data.description.substring(data.description.indexOf(":")+1, data.description.indexOf(",")).trim();
+      this.teacher = data.description.substring(data.description.lastIndexOf(":")+1).trim();
+      if (this.teacher.length<2){
+        this.teacher = "Giảng viên mời";
+      }
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
